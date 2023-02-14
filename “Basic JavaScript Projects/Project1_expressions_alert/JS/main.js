e1cb3045728f2cb1c01cbb4da99f981b64250eee
Maksims Sersnovs <maksimssir@gmail.com>
2023-02-14 00:01:16 +0000

    some changes

--- “Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ “Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,28 +1,23 @@
-alert("Hello World!");
-window.alert("Hello, World");
-document.write("Hello, world!");
+alert("Hello World!"); // alert method
+window.alert("Hello, World"); //window alert method
+document.write("Hello, world!"); //document write method
 
-var A = "This is a string!";
-document.write(A);
+var A = "Project!"; // variable A
+var B = "Maksims,Production!"; // variable B
+window.alert(A+B); //concatenated 2 string variables
 
-var B = "Maksims,Production!";
-window.alert(B);
+document.write("Lisa told Bart, \"\\Knock it Off, Bart! Or I'll tell dad!\"<br>\"Eat My shorts!\"Bart responded.");//escaping
 
-document.write("Lisa told Bart, \"\\Knock it Off, Bart! Or I'll tell dad!\"<br>\"Eat My shorts!\"Bart responded.");
+document.write ("\"Be who you are and say what you feel,"+"because those who mind don\'t matter and those who matter don\'t mind.\""+" Dr.Seuss");//Concatenating a String
 
-document.write (`\"Be who you are and say what you feel,because those who mind don\'t matter and those who matter don\'t mind.\" Dr.Seuss`);
+var Family = "The Arezzinis", Dad="Jeremiah", Mom="Hermione",Daughter="Penny", Son="Zorro"; //multiplie variables
+document.write(Son); //document write method
 
-var B = "\"Be who you are and say what you feel,because those who mind don\'t matter" + "Lisa told Bart, \"\\Knock it Off, Bart!"
-document.write(B)
+var blues = "I have the blues"; // variable string called blues
+var blues = blues.fontcolor("blue"); //str.fontcolor method
+document.write(blues); //document write method
 
-var Family = "The Arezzinis", Dad="Jeremiah", Mom="Hermione",Daughter="Penny", Son="Zorro";
-document.write(Son);
-
-var blues = "I have the blues";
-var blues = blues.fontcolor("blue");
-document.write(blues);
-
-Document.write(3 + 3);
+document.write(3 + 3); // statement containing an expression (3 + 3).
 
 function My_First_Function() {  //Definding a function and naming it
     var str="This Text is blue!"; //Definding a variable and giving it a //string value
